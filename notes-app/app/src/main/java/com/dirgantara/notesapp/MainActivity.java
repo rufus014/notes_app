8c84fc6150fbce4c701fa7439314ff1a0507cea1
Danil <firsoff.fir@gmail.com>
2022-06-10 00:06:29 +0300

    first commit

---
+++ notes-app/app/src/main/java/com/dirgantara/notesapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.dirgantara.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ListView noteListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) //отрисовка списка заметок
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initWidgets();
+        setNoteAdapter();
+        setOnClickListener();
+        loadFromDBToMemory();
+    }
+
+
+    private void initWidgets()
+    {
+        noteListView = findViewById(R.id.noteListView);
+    }
+
+    private void loadFromDBToMemory() //подгрузка заметок из памяти
+    {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        sqLiteManager.populateNoteListArray();
+    }
+
+    private void setNoteAdapter()
+    {
+        NoteAdapter noteAdapter = new NoteAdapter(getApplicationContext(), Note.nonDeletedNotes());
+        noteListView.setAdapter(noteAdapter);
+    }
+
+
+    private void setOnClickListener()
+    {
+        noteListView.setOnItemClickListener((adapterView, view, position, l) -> {
+            Note selectedNote = (Note) noteListView.getItemAtPosition(position);
+            Intent editNoteIntent = new Intent(getApplicationContext(), NoteDetailActivity.class);
+            editNoteIntent.putExtra(Note.NOTE_EDIT_EXTRA, selectedNote.getId());
+            startActivity(editNoteIntent);
+        });
+    }
+
+
+    public void newNote(View view) //новая заметка
+    {
+        Intent newNoteIntent = new Intent(this, NoteDetailActivity.class);
+        startActivity(newNoteIntent);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setNoteAdapter();
+    }
+}
